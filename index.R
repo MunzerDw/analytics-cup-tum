0d39e52536b7ce49f2322bda8d7be5c997e49729
MunzerDw <munzerdwedari@gmail.com>
2022-01-02 19:01:52 +0100

    preprocess columns

--- index.R
+++ index.R
@@ -74,6 +74,53 @@ df$MULTIPLE_OFFER <- if_else(!duplicated(df$MO_ID), 0, 1)
 ## Munzer: Every first
 ## +MO_CREATED_DATE
 
+# SALES_LOCATION - chr (nominal)
+
+# SERVICE_LIST_PRICE - num
+## no NAs
+
+# PRICE_LIST - chr (nominal)
+
+# MO_CREATED_DATE - date
+
+# SO_CREATED_DATE - date
+
+# BUSINESS_TYPE - chr (nominal)
+
+# OFFER_STATUS - chr (nominal)
+df <- df %>% mutate(
+  OFFER_STATUS=toupper(OFFER_STATUS)
+)
+df$OFFER_STATUS[df$OFFER_STATUS == "LOSE"] <- "LOST"
+df$OFFER_STATUS[df$OFFER_STATUS == "WIN"] <- "WON"
+
+# TEST_SET_ID - id/ num
+
+# SALES_BRANCH - chr (nominal)
+
+# chr (nominal)
+df <- df %>% mutate(
+  SALES_LOCATION = factor(SALES_LOCATION, labels = as.vector(unique(df$SALES_LOCATION))[!is.na(as.vector(unique(df$SALES_LOCATION)))]),
+  PRICE_LIST = factor(PRICE_LIST, labels = as.vector(unique(df$PRICE_LIST))),
+  BUSINESS_TYPE = factor(BUSINESS_TYPE, labels = as.vector(unique(df$BUSINESS_TYPE))),
+  OFFER_STATUS = factor(OFFER_STATUS, labels = as.vector(unique(df$OFFER_STATUS))[!is.na(as.vector(unique(df$OFFER_STATUS)))]),
+  SALES_BRANCH = factor(SALES_BRANCH, labels = as.vector(unique(df$SALES_BRANCH))[!is.na(as.vector(unique(df$SALES_BRANCH)))]),
+)
+
+# date
+df <- df %>% mutate(across(.cols = c(MO_CREATED_DATE, SO_CREATED_DATE), .fns = as_date))
+df %>% mutate(
+  across(
+    .cols = c(MCD=MO_CREATED_DATE),
+    .fns = c(WEEKDAY=wday, YEAR=year, MONTH=month, DAY=day, QUARTER=quarter)
+  )
+)
+df %>% mutate(
+  across(
+    .cols = c(SCD=SO_CREATED_DATE),
+    .fns = c(WEEKDAY=wday, YEAR=year, MONTH=month, DAY=day, QUARTER=quarter)
+  )
+)
 
 summary(df)
 
